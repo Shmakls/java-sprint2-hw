1e62688db4689d54526e37f4c6d2156f72873250
Artur A <nakls@inbox.ru>
2022-04-08 15:47:07 +0300

    Перенос с рабочего на домашний 08.04.22

--- src/ru/andrianov/TaskManager.java
+++ src/ru/andrianov/TaskManager.java
@@ -6,18 +6,17 @@ import ru.andrianov.operations.EpicStatus;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 public class TaskManager {
 
     private TaskRepository taskRepository;
     private EpicStatus epicStatus;
-    private HistoryRepository historyManager;
+    private HistoryRepository historyRepository;
 
     public TaskManager(TaskRepository taskRepository, HistoryRepository historyManager) {
         this.taskRepository = taskRepository;
         epicStatus = new EpicStatus();
-        this.historyManager = historyManager;
+        this.historyRepository = historyManager;
     }
 
     public Integer createNewTask(Task task) {
@@ -45,91 +44,102 @@ public class TaskManager {
     public void printAllTasks() {
         Map<Integer, Task> tasks = taskRepository.getTasks();
         System.out.println("");
-        System.out.print("Вывожу список всех задач:");
-        for (Integer taskId : tasks.keySet()) {
-            Task task = tasks.get(taskId);
+        if (tasks.size() > 0) {
+            System.out.print("Вывожу список всех задач:");
+            for (Integer taskId : tasks.keySet()) {
+                Task task = tasks.get(taskId);
+                System.out.println("");
+                System.out.print(task);
+            }
+
             System.out.println("");
-            System.out.print(task);
+        } else {
+            System.out.println("Список задач пуст.");
         }
-        System.out.println("");
         System.out.println("");
     }
 
     public void clearAllTasks() {
         taskRepository.clearAllTasks();
-        historyManager.clear();
+        historyRepository.clear();
         System.out.println("Все задачи удалены!");
     }
 
     public Task getTaskById(int taskId) {
-        Task task = taskRepository.getTaskById(taskId);
-        System.out.println("Задача \"" + task.getTitle() + "\" получена.");
-        historyManager.add(task);
-        return task;
+        if (taskRepository.getTasks().containsKey(taskId)) {
+            Task task = taskRepository.getTaskById(taskId);
+            historyRepository.add(task);
+            return task;
+        } else {
+            throw new IllegalArgumentException("Такого ID в списке нет");
+        }
     }
 
     public void removeTaskById(int taskId) {
-        Task task = taskRepository.getTaskById(taskId);
-        int epicTaskId = 0;
+        if (taskRepository.getTasks().containsKey(taskId)) {
+            Task task = taskRepository.getTaskById(taskId);
+            int epicTaskId = 0;
 
-        if (task instanceof Epic) {
-            List<Integer> subtasksIds = ((Epic) task).getSubtasksIds();
-            for (Integer subtaskId : subtasksIds) {
-                taskRepository.removeTaskById(subtaskId);
-                historyManager.removeTaskFromHistoryById(subtaskId);
+            if (task instanceof Epic) {
+                List<Integer> subtasksIds = ((Epic) task).getSubtasksIds();
+                for (Integer subtaskId : subtasksIds) {
+                    taskRepository.removeTaskById(subtaskId);
+                    historyRepository.removeTaskFromHistoryById(subtaskId);
+                }
+                System.out.println("Все связанные подзадачи удалены!");
             }
-            System.out.println("Все связанные подзадачи удалены!");
-        }
 
-        if (task instanceof Subtask) {
-            epicTaskId = ((Subtask) task).getEpicTaskId();
-            Epic epicTask = (Epic) taskRepository.getTaskById(epicTaskId);
+            if (task instanceof Subtask) {
+                epicTaskId = ((Subtask) task).getEpicTaskId();
+                Epic epicTask = (Epic) taskRepository.getTaskById(epicTaskId);
 
-            List<Integer> subtasksIds = epicTask.getSubtasksIds();
-            subtasksIds.remove(taskId);
+                List<Integer> subtasksIds = epicTask.getSubtasksIds();
+                subtasksIds.remove((Integer) taskId);
 
-            System.out.println("Связь subtask с epic задачей удалена!");
-        }
+                System.out.println("Связь subtask с epic задачей удалена!");
+            }
 
-        String title = task.getTitle();
-        taskRepository.removeTaskById(taskId);
-        System.out.println("Задача \"" + title + "\" удалена!");
+            String title = task.getTitle();
+            taskRepository.removeTaskById(taskId);
+            System.out.println("Задача \"" + title + "\" удалена!");
 
-        historyManager.removeTaskFromHistoryById(taskId);
+            historyRepository.removeTaskFromHistoryById(taskId);
 
-        if (task instanceof Subtask) {
-            epicStatus.checkAndChangeEpicStatus(taskRepository, epicTaskId);
+            if (task instanceof Subtask) {
+                epicStatus.checkAndChangeEpicStatus(taskRepository, epicTaskId);
+            }
+        } else {
+            throw new IllegalArgumentException("Такого ID в списке нет");
         }
 
     }
 
     public void updateTask(Task task, int taskId) {
 
-        int epicTaskId = 0;
-        List<Integer> subtasksIds = null;
+            int epicTaskId = 0;
+            List<Integer> subtasksIds = null;
 
-        if (task instanceof Epic) {
-            subtasksIds = ((Epic) task).getSubtasksIds();
-        }
+            if (task instanceof Epic) {
+                subtasksIds = ((Epic) task).getSubtasksIds();
+            }
 
-        if (task instanceof Subtask) {
-            epicTaskId = ((Subtask) task).getEpicTaskId();
-        }
+            if (task instanceof Subtask) {
+                epicTaskId = ((Subtask) task).getEpicTaskId();
+            }
 
-        task.setId(taskId);
+            task.setId(taskId);
 
-        taskRepository.updateTask(task, taskId);
-        System.out.println("Задача с ID " + taskId + " обновлена.");
+            taskRepository.updateTask(task, taskId);
+            System.out.println("Задача с ID " + taskId + " обновлена.");
 
-        if (task instanceof Epic) {
-            Epic epicTask = (Epic) taskRepository.getTaskById(taskId);
-            epicTask.setSubtasksIds(subtasksIds);
-        }
+            if (task instanceof Epic) {
+                Epic epicTask = (Epic) taskRepository.getTaskById(taskId);
+                epicTask.setSubtasksIds(subtasksIds);
+            }
 
-        if (task instanceof Subtask) {
-            epicStatus.checkAndChangeEpicStatus(taskRepository, epicTaskId);
-        }
-
+            if (task instanceof Subtask) {
+                epicStatus.checkAndChangeEpicStatus(taskRepository, epicTaskId);
+            }
     }
 
     public void getSubtaskListByEpic(int epicId) {
@@ -149,7 +159,7 @@ public class TaskManager {
     }
 
     public void printHistory() {
-        Collection<Task> viewedTasks = historyManager.getHistory();
+        Collection<Task> viewedTasks = historyRepository.getHistory();
         System.out.println("Вывожу историю просмотренных задач:");
         if (viewedTasks != null) {
             for (Task viewedTask : viewedTasks) {
@@ -158,6 +168,18 @@ public class TaskManager {
             System.out.println("");
         } else {
             System.out.println("История просмотров пуста");
+        }
+    }
+
+    public Integer getAmountOfStoredTasks() {
+        return taskRepository.getTasks().size();
+    }
+
+    public Integer getAmountOfStoredHistory() {
+        if (historyRepository.getHistory() != null) {
+            return historyRepository.getHistory().size();
+        } else {
+            return 0;
         }
     }
 }
--- test/ru/andrianov/TaskManagerTest.java
+++ test/ru/andrianov/TaskManagerTest.java
@@ -8,6 +8,8 @@ import ru.andrianov.data.Status;
 import ru.andrianov.data.Subtask;
 import ru.andrianov.data.Task;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskManagerTest {
@@ -27,6 +29,14 @@ class TaskManagerTest {
         epic1 = new Epic("TestEpic1", "DescriptionTestEpic1", Status.NEW);
         subtask1 = new Subtask("TestSubtask1", "DescriptionTestSubtask1", Status.NEW, 3);
         subtask2 = new Subtask("TestSubtask2", "DescriptionTestSubtask2", Status.IN_PROGRESS, 3);
+    }
+
+    void addFiveTestTasksToTaskManager() {
+        taskManager.createNewTask(task1);
+        taskManager.createNewTask(task2);
+        taskManager.createNewTask(epic1);
+        taskManager.createNewTask(subtask1);
+        taskManager.createNewTask(subtask2);
     }
 
     @Test
@@ -69,6 +79,207 @@ class TaskManagerTest {
 
         assertEquals(3, epicIdFromSubtask1, "Связь между эпик задачей и подзадачей не установлена.");
         assertEquals(4, testSubtask1IdFromEpic, "В эпик не записалась связь с подзадачей.");
+    }
+
+    @Test
+    void shouldBeChangeEpicTaskStatusWhenSubtaskStatusChanged() {
+        Integer task1Id = taskManager.createNewTask(task1);
+        Integer task2Id = taskManager.createNewTask(task2);
+        Integer epicId = taskManager.createNewTask(epic1);
+        Integer subtask1Id = taskManager.createNewTask(subtask1);
+        Integer subtask2Id = taskManager.createNewTask(subtask2);
+
+        Epic epicFromRepository = (Epic) taskManager.getTaskById(epicId);
+        Status epicTestStatusInProgress = epicFromRepository.getStatus();
+
+        assertEquals(Status.IN_PROGRESS, epicTestStatusInProgress);
+
+        Subtask subtask2FromRepository = (Subtask) taskManager.getTaskById(subtask2Id);
+        subtask2FromRepository.setStatus(Status.NEW);
+        taskManager.updateTask(subtask2FromRepository, subtask2Id);
+        Status epicTestStatusNew = epicFromRepository.getStatus();
+
+        assertEquals(Status.NEW, epicTestStatusNew);
+
+        Subtask subtask1FromRepository = (Subtask) taskManager.getTaskById(subtask1Id);
+        subtask1FromRepository.setStatus(Status.DONE);
+        subtask2FromRepository.setStatus(Status.DONE);
+        taskManager.updateTask(subtask1FromRepository, subtask1Id);
+        taskManager.updateTask(subtask2FromRepository, subtask2Id);
+
+        Status epicTestStatusDone = epicFromRepository.getStatus();
+
+        assertEquals(Status.DONE, epicTestStatusDone);
+
+    }
+
+    @Test
+    void shouldBePrintTaskListWithFiveTasks() {
+        addFiveTestTasksToTaskManager();
+
+        assertEquals(5, taskManager.getAmountOfStoredTasks());
+
+        taskManager.printAllTasks();
+
+        ;
+
+
+    }
+
+    @Test
+    void shouldBeClearTaskRepositoryIfNoTasksSaved() {
+        assertEquals(0, taskManager.getAmountOfStoredTasks());
+
+        taskManager.clearAllTasks();
+
+        assertEquals(0, taskManager.getAmountOfStoredTasks());
+    }
+
+    @Test
+    void shouldBeClearTaskRepositoryWhenUsedClearAllTasksWithTasks() {
+        addFiveTestTasksToTaskManager();
+
+        assertEquals(5, taskManager.getAmountOfStoredTasks());
+
+        taskManager.clearAllTasks();
+
+        assertEquals(0, taskManager.getAmountOfStoredTasks());
+
+    }
+
+    @Test
+    void shouldBeClearHistoryRepositoryWhenClearAllTasks() {
+        addFiveTestTasksToTaskManager();
+
+        assertEquals(5, taskManager.getAmountOfStoredTasks());
+
+        Task callTask1 = taskManager.getTaskById(1);
+        Task callEpic1 = taskManager.getTaskById(3);
+
+        assertEquals(2, taskManager.getAmountOfStoredHistory());
+
+        taskManager.clearAllTasks();
+
+        assertEquals(0, taskManager.getAmountOfStoredHistory());
+
+    }
+
+    @Test
+    void shouldBeGetTaskByIdIfTaskPresent() {
+        addFiveTestTasksToTaskManager();
+
+        assertEquals(5, taskManager.getAmountOfStoredTasks());
+
+        Task testTask1 = taskManager.getTaskById(1);
+        Task testSubtask2 = taskManager.getTaskById(5);
+
+        assertEquals(task1.getId(), testTask1.getId());
+        assertEquals(subtask2.getId(), testSubtask2.getId());
+
+    }
+
+    @Test
+    void shouldBeThrowExceptionIfGetIdNotExist() {
+        final IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> taskManager.getTaskById(10)
+        );
+
+        assertEquals("Такого ID в списке нет", exception.getMessage());
+
+
+    }
+
+    @Test
+    void shouldBeRemoveTaskFromRepository() {
+        addFiveTestTasksToTaskManager();
+
+        assertEquals(5, taskManager.getAmountOfStoredTasks());
+
+        taskManager.removeTaskById(1);
+
+        final IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> taskManager.getTaskById(1)
+        );
+
+        assertEquals("Такого ID в списке нет", exception.getMessage());
+    }
+
+    @Test
+    void shouldBeRemoveEpicAndHisSubtasksFromRepository() {
+        addFiveTestTasksToTaskManager();
+
+        assertEquals(5, taskManager.getAmountOfStoredTasks());
+
+        taskManager.removeTaskById(3);
+
+        final IllegalArgumentException epic1Exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> taskManager.getTaskById(3)
+                );
+
+        assertEquals("Такого ID в списке нет", epic1Exception.getMessage());
+
+        final IllegalArgumentException subtask1Exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> taskManager.getTaskById(4)
+        );
+
+        assertEquals("Такого ID в списке нет", subtask1Exception.getMessage());
+
+        final IllegalArgumentException subtask2Exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> taskManager.getTaskById(5)
+        );
+
+        assertEquals("Такого ID в списке нет", subtask2Exception.getMessage());
+    }
+
+    @Test
+    void shouldBeRemoveSubtaskAndHisConnectionWithEpic() {
+        addFiveTestTasksToTaskManager();
+
+        assertEquals(5, taskManager.getAmountOfStoredTasks());
+
+        taskManager.removeTaskById(5);
+
+        assertEquals(4, taskManager.getAmountOfStoredTasks());
+
+        Epic epic1Test = (Epic) taskManager.getTaskById(3);
+        List<Integer> subtasksId = epic1Test.getSubtasksIds();
+
+        assertFalse(subtasksId.contains(5));
+
+    }
+
+    @Test
+    void shouldBeThrowExceptionIfRemoveIdNotExist() {
+        addFiveTestTasksToTaskManager();
+
+        final IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> taskManager.removeTaskById(10)
+        );
+
+        assertEquals("Такого ID в списке нет", exception.getMessage());
+
+    }
+
+    @Test
+    void shouldBeUpdateTaskIfGiveTask() {
+        addFiveTestTasksToTaskManager();
+
+        assertEquals(5, taskManager.getAmountOfStoredTasks());
+
+        Task updateTask1 = new Task("UpdateTask1", "DescriptionUpdateTask1", Status.NEW);
+
+        taskManager.updateTask(updateTask1, 1);
+
+        Task testTask = taskManager.getTaskById(1);
+
+        assertEquals("UpdateTask1", testTask.getTitle());
+
     }
 
 }
